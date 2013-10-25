2c348749996ba04bd873f2caa29b5bb0dc4d82f2
Tuomas Starck <dev@starck.fi>
2013-10-26 01:01:57 +0300

    Switch to branches

--- autodeploy/init.sh
+++ autodeploy/init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 npm config set prefix $PREFIX --global
 npm install
@@ -6,6 +6,4 @@ npm link
 
 install -m 0644 $DEPLOYDIR/$INTENT.conf /etc/init
 
-ln -sf /usr/local/mbin/portforward/conf/provided_services.config /etc/pollbal.conf
-
-initctl emit --no-wait theservicestart
+service $INTENT start
--- autodeploy/update.sh
+++ autodeploy/update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-git clean -f
-git reset --hard HEAD
-git checkout master
-git pull
+. $DEPLOYDIR/stage1.sh
+
+git_upgrade && {
+    say Version has not changed, exiting
+    exit 0
+}
 
 npm update
 
+say Installing Upstart script
 install -m 0644 $DEPLOYDIR/$INTENT.conf /etc/init
 
-initctl emit --no-wait theservicestart
+say Restarting service
+service $INTENT restart
+
+say Githupdate done.
